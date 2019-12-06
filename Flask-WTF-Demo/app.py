760b1d8b2b4e13395ba251dad007c3af40c84b86
redsnowc <nuanyang.44@gmail.com>
2019-12-06 13:46:46 +0800

    Flask-WTF 表单示例

--- Flask-WTF-Demo/app.py
+++ Flask-WTF-Demo/app.py
@@ -1,1 +1,31 @@
+from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
 
+from forms import RegisterForm
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "1234"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1234@localhost:3306/fortest?charset=UTF8MB4'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+@app.route("/", methods=["POST", "GET"])
+def index():
+    form = RegisterForm()
+    print(request.method)
+    if form.validate_on_submit():
+        from models import User
+        user = User()
+        user.username = form.username.data
+        user.email = form.email.data
+        user.age = form.age.data
+        user.password = form.password.data
+        db.session.add(user)
+        db.session.commit()
+    return render_template("form.html", form=form)
+
+
+if __name__ == "__main__":
+    app.run()
